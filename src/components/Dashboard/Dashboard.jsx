52ef3000131097116e20868e914e503b38a3dac0
Race428 <raceacheson@gmail.com>
2019-06-04 13:35:42 -0600

    added admindash

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -10,6 +10,7 @@ import {withRouter} from 'react-router-dom'
 import {updateUser} from '../../redux/reducer'
 import {connect} from 'react-redux'
 import UserDashboard from './UserDashboard/UserDashboard'
+import AdminDashboard from './AdminDashboard/AdminDashboard'
 
 class Dashboard extends Component {
     constructor() {
@@ -43,7 +44,7 @@ class Dashboard extends Component {
             <div className='dashboardContainer'>
                 {
                     this.state.admin ? 
-                    <h1>This is the admin dashboard</h1> :
+                    <AdminDashboard />  :
                    <UserDashboard /> 
                 }
             </div>
