07f23ce12ce0d0204ec55d7ba0ac81bc9b850384
Mike De Groot <mdegroot09@gmail.com>
2019-06-04 11:47:01 -0600

    added deleteUserJob ctrl method and sql query

--- server/controllers/userCtrl.js
+++ server/controllers/userCtrl.js
@@ -27,5 +27,25 @@ module.exports = {
     catch {
       res.status(500).send('Internal server error')
     }
+  },
+
+  deleteUserJob: async (req, res) => {
+    let user_id = req.session.user.id
+    let job_id = req.params.id
+    const db = req.app.get('db')
+
+    await db.userCtrl.deleteUserJob({user_id, job_id})
+
+    let userJobArr = await db.userCtrl.getUserJob({user_id, job_id})
+    let userJob = userJobArr[0]
+
+    try {
+      if(!userJob){
+        res.sendStatus(200)
+      } 
+    }
+    catch {
+      res.status(500).send('Internal server error')
+    }
   }
 }
--- server/index.js
+++ server/index.js
@@ -50,8 +50,8 @@ app.post('/api/postings/new', adminCtrl.newPosting)
 // app.put('/api/admins/:id', adminCtrl.updateAdmin)
 
 // users
-app.get('/api/jobs', userCtrl.getUserJobs) //jobs the user's applied for including viedo urls
-app.get('/api/jobs/:id', userCtrl.getUserJob)// get single job applied for
-// delete video
-// update video
+app.get('/api/jobs', userCtrl.getUserJobs)
+app.get('/api/jobs/:id', userCtrl.getUserJob)
+app.delete('/api/jobs/:id', userCtrl.deleteUserJob)
+// app.put('')
 // add video
