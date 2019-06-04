a04837b0e2fd79062ad7060a581a6b12ae81171c
JoshuarTaylor2015 <joshuartaylor2015@gmail.com>
2019-06-04 12:04:33 -0600

    added guts to admindashboard component

--- src/components/Dashboard/AdminDashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard/AdminDashboard.jsx
@@ -1,18 +1,122 @@
-// functionality needed: 
-// firstly: determine whether or not they're an owner or an admin
+import React, {Component} from 'react'
+import axios from 'axios'
+import {withRouter} from 'react-router-dom'
+import {updateUser} from '../../redux/reducer'
+import {connect} from 'react-redux'
 
-    // admin functionality: 
-    // function to grab company info
+class AdminDashboard extends Component {
+    constructor() {
+        super()
+        this.state = {
+            companyInfoPlaceholder: [],
+            jobListings: [],
+            administrators: []
+        }
+    }
+            
+    async componentDidMount() {
+    // grab session if there is one- if not, push to login ~~~~~~~~~~
+        // const session = await axios.get('/auth/session') 
+        // if(session) {
+            // await this.props.updateUser(session.data)
+            // } else {
+                // this.props.history.push('/')
+            // }
+
+    // if(!this.props.companyId)
+        // this.props.history.push('/login)
+      
+    // else if(this.props.companyId): ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~     
+        // this.getListings() gets the admin's job listings
     
-    // function to grab job listings they've made
-    // function to add job listing they've made
-    // function to edit job listing they've made
-    // function to delete job listing they've made
+    // else if(this.props.owner): ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+         // this.getAllListings() gets the company's job listings 
+         // this.getAdmins() gets the company's admins
+            
+    // function to grab company info
+        // nothing yet
+    }
 
+    // admin functionality ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  
+    getListings=async ()=> {
+        const listings = await axios.get('/api/postings/admin')
+        this.setState ({
+            jobListings: listings.data
+        })
+    }              
+    addJob=()=> {
+        axios.post('/api/postings/new').then(res => {
+            this.props.owner ? 
+            await this.getAllListings() :
+            await this.getListings()
+        })
+    }
+    updateJob=async (id)=> {
+        axios.put(`/api/postings/${id}`).then(res => {
+            this.props.owner ? 
+            await this.getAllListings() :
+            await this.getListings()
+        })
+    }
+    deleteJob=async (id)=> {
+        axios.delete(`/api/postings/${id}`).then(res => {
+            this.props.owner ? 
+            await this.getAllListings() :
+            await this.getListings()
+        })
+    }
     // function to view application for job listing
+        // nothing yet
     // function to contact applicant for follow-up interview
+        // nothing yet
 
-    // additional owner functionality: 
-    // function to grab ALL job listings for the company
-    // function to grab ALL admins in the company
-    // function to remove admins from the company
+
+    // owner functionality ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   
+    getAllListings=async ()=> {
+        const listings = await axios.get('/api/postings/company')
+        this.setState ({
+            jobListings: listings.data
+        })
+    }
+    getAdmins=async ()=> {
+        const admins = await axios.get(`/api/admins/${companyId}`)
+        this.setState ({
+            administrators: admins.data
+        })
+    }
+    deleteAdmin=async (id)=> {
+        axios.delete(`/api/admins/${id}`).then(res=> {
+            await this.getAdmins();
+        })
+    }
+
+    render() {
+        return (
+            <div className='adminDashboardContainer'>
+                <h1>This is the admin dashboard</h1>
+                {
+                    this.props.owner ? 
+                    <h2>You see this if you're the owner</h2> :
+                    <> </>
+                }
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    const {companyId, email, id, firstName, lastName} = state
+    return {
+        companyId, 
+        firstName,
+        lastName,
+        email,  
+        id
+    }
+}
+
+const mapDispatchToProps = {
+    updateUser
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(AdminDashboard))
